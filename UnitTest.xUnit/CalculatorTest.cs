2c90eb295a062dacabd5ee748d32c86e6d817aaa
Umut Atraş <umutatras@outlook.com>
2024-08-15 12:13:57 +0300

    basic unittest

---
+++ UnitTest.xUnit/CalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.APP;
+
+namespace UnitTest.xUnit
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            // bir nesne örneği oluşturma kısmı arrange evresinde yapılır
+            int a = 5;
+            int b = 20;
+            var calcultor = new Calculator();
+
+            //Act
+            //Oluşturulan nesnelerin parametrelerini verildiği evredir.
+            var total = calcultor.add(a, b);
+
+            //Assert
+            //Verimizi çalışıp çalışmadığını test ettiğimiz yerdir
+
+            Assert.Equal<int>(25, total);
+
+        }
+    }
+}
