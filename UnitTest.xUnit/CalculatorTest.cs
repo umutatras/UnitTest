860eb584192de839f17d474bcd5f829fd2aba1ee
Umut Atraş <umutatras@outlook.com>
2024-08-16 12:03:02 +0300

    matc dosnotmatch

--- UnitTest.xUnit/CalculatorTest.cs
+++ UnitTest.xUnit/CalculatorTest.cs
@@ -26,11 +26,19 @@ namespace UnitTest.xUnit
             ////Verimizi çalışıp çalışmadığını test ettiğimiz yerdir
 
             //Assert.Equal<int>(25, total);
-            var names=new List<string>() { "Fatih","Umut","Emre"};
-            Assert.Contains(names, x => x == "Fatih");//liste içinde fatih olan var mı diye bakar.
-            Assert.Contains("fatih", "fatih çakıroğlu");//içerisinde beklenen değeri arar. true döner çünkü asıl değerimizde fatih ismi vardır.
-            Assert.DoesNotContain("emre", "fatih çakıroğlu");//içerisinde beklenen değeri içermemesini arar. true döner çünkü asıl değerimizde emre ismi yoktur.
+            //var names=new List<string>() { "Fatih","Umut","Emre"};
+            //Assert.Contains(names, x => x == "Fatih");//liste içinde fatih olan var mı diye bakar.
+            //Assert.Contains("fatih", "fatih çakıroğlu");//içerisinde beklenen değeri arar. true döner çünkü asıl değerimizde fatih ismi vardır.
+            //Assert.DoesNotContain("emre", "fatih çakıroğlu");//içerisinde beklenen değeri içermemesini arar. true döner çünkü asıl değerimizde emre ismi yoktur.
 
+            //Assert.True(5 > 2);//şarta göre başarılır olur 
+            //Assert.False(5 < 2);//şarta göre başarılır olur 
+            //Assert.True("".GetType() == typeof(string));//şarta göre başarılır olur 
+
+            ////regex parametreleri alır.
+            //var regex = "^dog";
+            //Assert.Matches(regex, "dog Umut");
+            //Assert.DoesNotMatch(regex, "umut Dog");
         }
     }
 }
