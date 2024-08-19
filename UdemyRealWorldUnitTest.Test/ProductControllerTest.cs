96e97750fa1a8378f453bc91bc68011e625d56ee
Umut Atraş <umutatras@outlook.com>
2024-08-19 13:39:08 +0300

    Details_IdInvalid_ReturnNotFound method yazıldı

--- UdemyRealWorldUnitTest.Test/ProductControllerTest.cs
+++ UdemyRealWorldUnitTest.Test/ProductControllerTest.cs
@@ -56,5 +56,16 @@ namespace UdemyRealWorldUnitTest.Test
             Assert.Equal("Index",redirect.ActionName);
         }
 
+        [Fact]
+        public async void Details_IdInvalid_ReturnNotFound()
+        {
+            Product product=null;
+            _mockRepo.Setup(x => x.GetById(0)).ReturnsAsync(product);
+            var result = await _controller.Details(0);
+            var redirect=Assert.IsType<NotFoundResult>(result);
+
+            Assert.Equal<int>(404, redirect.StatusCode);
+        }
+
     }
 }
