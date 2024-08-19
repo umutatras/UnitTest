c53786c66a7e49d2dad7f5af7190098f9ead18a5
Umut Atraş <umutatras@outlook.com>
2024-08-19 11:57:48 +0300

    Test projesi oluşturuldu ctor ayarları yapıldı

---
+++ UdemyRealWorldUnitTest.Test/ProductControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using UdemyRealWordUnitTest.Web.Controllers;
+using UdemyRealWordUnitTest.Web.Models;
+using UdemyRealWordUnitTest.Web.Repository;
+using Xunit;
+
+namespace UdemyRealWorldUnitTest.Test
+{
+
+    public class ProductControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+        private readonly ProductsController _controller;
+        private List<Product> products;
+        public ProductControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _controller = new ProductsController(_mockRepo.Object);
+            products = new List<Product>() { new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 50, Color = "Kırmızı" },
+                new Product { Id = 2, Name = "Kalem2", Price = 150, Stock = 250, Color = "Mavi" } };
+        }
+
+        [Fact]
+        public async void Index_ActionExecutes_ReturnView()
+        {
+            var result=await _controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+    }
+}
